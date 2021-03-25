595279ae7bce7b641635db7c98dba863b84b5ce4
Oggup <edifoo@gmail.com>
2021-03-25 16:38:58 -0700

    added initial files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { App } from "./components";
 
-ReactDOM.render(
-  <App />,
+const App = () => {
+  return (
+    <>
+      <h3>Hello world!</h3>
+    </>
+  );
+};
 
-  document.querySelector("#app")
-);
+export default App;
