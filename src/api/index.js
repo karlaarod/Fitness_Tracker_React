e1eb460beabbe2532efdf8600e083d6c4459351c
Oggup <edifoo@gmail.com>
2021-03-25 19:36:00 -0700

    sample message

--- src/api/index.js
+++ src/api/index.js
@@ -15,9 +15,9 @@ export const callApi = async ({ url, method, token, body }) => {
     if (token) {
       options.headers["Authorization"] = `Bearer ${token}`;
     }
-    console.log("request url: ", API_URL + url);
+    console.log("request url: ", BASE_URL + url);
     console.log("options: ", options);
-    const response = await fetch(API_URL + url, options);
+    const response = await fetch(BASE_URL + url, options);
     const data = await response.json();
     console.log("data: ", data);
     if (data.error) throw data.error;
