a29e89313475f409dd2499534dc84ecd0e01c228
Karla Rodriguez <karla0611@gmail.com>
2021-03-26 13:22:55 -0700

    api and app updated to match login branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,87 @@
 import React, { useState, useEffect } from "react";
-import ReactDom from 'react-dom';
+import ReactDom from "react-dom";
 import {
+  BrowserRouter as Router,
   Route,
   useHistory,
   Switch,
   Link,
 } from "react-router-dom";
 import { NavBar, Routines, Activities, Dashboard, Account } from ".";
+import { callApi } from "../api";
+
 
 const fetchUserData = async (token) => {
   const { data } = await callApi({
-    url: "/users/me",
+    url: "users/me",
     token,
   });
+
   return data;
 };
 
-const fetchActivities = async () => {
-  const {
-    data: { activities},
-  } = await callApi({
-    url: "/activities",
-  });
-  return activities;
-};
+// const fetchActivities = async () => {
+//   const {
+//     data: { activities},
+//   } = await callApi({
+//     url: "/activities",
+//   });
+//   return activities;
+// };
 
 const App = () => {
   const [token, setToken] = useState("");
   const [userData, setUserData] = useState({});
-  const [activities, setActivities] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // const [activities, setActivities] = useState([]);
 
   useEffect(async () => {
-    const activitiess = await fetchActivities();
-    if (activities){ 
-      setActivities(activities);
-    }
+    // const activitiess = await fetchActivities();
+    // if (activities){
+    //   setActivities(activities);
+    // }
     if (!token) {
       setToken(localStorage.getItem("token"));
+      setIsLoggedIn(false)
       return;
     }
     const data = await fetchUserData(token);
     if (data && data.username) {
       setUserData(data);
-    }}, [token]);
-
-
-
+    }
+  }, [token]);
+  console.log(`Token is: ${token}`);
+  console.log("userData", userData);
+  
   return (
     <>
       <h3>This is inside the app container</h3>
-      <NavBar />
+      <NavBar 
+        userData= {userData} 
+        setToken={setToken} 
+        setUserData={setUserData} />
+      <Route path = "/routines">
       <Routines />
-      <Route path="/activities">
-      <Activities 
-        activities = {activities}
-      />
-      {console.log('activity:', activities)}
       </Route>
+      <Route path ="/activities">
+      <Activities />
+      </Route>
+      <Route path = "/dashboard">
       <Dashboard />
-      {/* <Route path="/login"> */}
-        <Account action="login" setToken={setToken} setUserData={setUserData} />
-      {/* </Route> */}
-      {/* <Route path="/register"> */}
+      </Route>
+      <Route path="/login">
+        <Account 
+          action="login" 
+          setToken={setToken} 
+          setUserData={setUserData} />
+      </Route>
+      <Route path="/register">
         <Account
           action="register"
           setToken={setToken}
           setUserData={setUserData}
         />
-      {/* </Route> */}
+      </Route>
     </>
   );
 };
