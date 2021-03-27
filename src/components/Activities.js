a8a7588740b4eca821878cb204e318f329bdf6dd
Karla Rodriguez <karla0611@gmail.com>
2021-03-26 19:17:51 -0700

    added comments

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
 const Activities = ( {activities} ) => {
-  const history = useHistory();
+
 
 //   console.log ('activities.js', activities)
 
+// return an array of activities created
 
   return (<>
     <h2>Activities</h2>
 
 <div className= "activites-list">
+{/* if activities exist map over and display activity name and description. 
+If no activities, display message */}
+
      { activities ? (
                 activities.map(( activity ) => (
                     <div key= {activity.id} className= 'activity'>
                 <h4>{activity.name}</h4>
                 <div>Description: {activity.description} </div>
-                {/* <button
-                    onClick={() => {
-                    histor .push(`/activities/${activity.id}`);
-                    }}>View Activity</button> */}
                     </div>
                     ))
                 ) : (
