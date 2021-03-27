88d9865de0bf92aab1d299be65a7de843f3a1bd0
Oggup <edifoo@gmail.com>
2021-03-26 21:50:56 -0700

    cleaning up the code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 
 const Dashboard = ({ userData }) => {
+    if (!userData.id) {
+        return (
+          <div className="sign-in-message">
+            <h1>Please <Link to="/login">log in</Link> to view your dashboard</h1>
+            
+          </div>
+        );
+      }
   return (
     <>
       <div className="dashboard">{<h1>Hello, {userData.username}!</h1>}</div>
